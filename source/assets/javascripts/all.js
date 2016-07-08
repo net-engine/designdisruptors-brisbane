0e158e50e6e35bacd9bd84a123422f51ee261099
Felix Lee <felix@netengine.com.au>
2016-07-08 16:46:12 +1000

    Reponsive fix

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,13 @@
 // This is where it all goes :)
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+// });
+function showMenu(e) {
+  document.getElementsByClassName("js-menu")[0].classList.add("transformY-0");
+  document.body.classList.add("overflow-hidden");
+}
+
+function hideMenu(e) {
+  document.getElementsByClassName("js-menu")[0].classList.remove("transformY-0");
+  document.body.classList.remove("overflow-hidden");
+}
